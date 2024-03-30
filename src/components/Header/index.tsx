0f84da2cbf85cd43f0ac0e013acf48aaf68bd5dc
Gabinks <gabin.buignet@gmail.com>
2024-03-30 15:15:42 +0100

    Changed <a> to <Link>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export function Header(){
     return (
         <header className="bg-slate-700 py-5 px-10 flex justify-between items-center">
             <p className="text-3xl text-white">ValorantDB</p>
             <ul className="text-lg flex gap-5 text-white">
-                <li><a href="/" className="hover:opacity-80">Home</a></li>
-                <li><a href="/news" className="hover:opacity-80">News</a></li>
-                <li><a href="/login" className="hover:opacity-80">Login</a></li>
+                <li><Link href="/" className="hover:opacity-80">Home</Link></li>
+                <li><Link href="/news" className="hover:opacity-80">News</Link></li>
+                <li><Link href="/login" className="hover:opacity-80">Login</Link></li>
             </ul>
         </header>
     )
