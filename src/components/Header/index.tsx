06074ffcf16dccf94fa03175e25951d253c235f6
Gabinks <gabin.buignet@gmail.com>
2024-04-21 17:52:28 +0200

    Added header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,40 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export function Header(){
+export function Header() {
+    const [logged, setLogged] = useState(false)
+    const [loaded, setLoaded] = useState(false);
+    const [token, setToken] = useState("");
+    const path = usePathname()
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            setLogged(true)
+            setToken(token)
+        }
+        else {
+            setLogged(false)
+        }
+        setLoaded(true)
+    }, [path, token]);
     return (
-        <header className="bg-slate-700 py-5 px-10 flex justify-between items-center">
-            <p className="text-3xl text-white">ValorantDB</p>
-            <ul className="text-lg flex gap-5 text-white">
+        <header className="bg-slate-700 py-5 px-10 flex mobile:flex-col justify-between items-center mobile:gap-5">
+            <Link href="/"><p className="text-3xl text-white">ValorantDB</p></Link>
+            <ul className="text-lg flex mobile:flex-col gap-5 text-white">
                 <li><Link href="/" className="hover:opacity-80">Home</Link></li>
                 <li><Link href="/news" className="hover:opacity-80">News</Link></li>
-                <li><Link href="/login" className="hover:opacity-80">Login</Link></li>
+                {!loaded ? (
+                    <li className="animate-pulse">Loading...</li>
+                ) : (
+                    logged ? (
+                        <li><Link href="/profile" className="hover:opacity-80">Profile</Link></li>
+
+                    ) : (
+                        <li><Link href="/login" className="hover:opacity-80">Login</Link></li>
+                    )
+                )}
             </ul>
         </header>
     )
