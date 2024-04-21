0ac039923e7f8f74d57770c7af33f444a3776f51
Gabinks <gabin.buignet@gmail.com>
2024-04-21 17:50:40 +0200

    Added Loading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import {useEffect} from "react";
 import {useRouter} from "next/navigation";
+import {Loading} from "@/components/Loading";
 
 export default function Home(){
     const router = useRouter()
@@ -9,7 +10,7 @@ export default function Home(){
     }, []);
     return (
         <main>
-
+            <Loading/>
         </main>
     )
 }
