3b83968ce9edb2e4a6bfc098e08c17e7ffe1d5af
Gabinks <gabin.buignet@gmail.com>
2024-04-21 17:53:43 +0200

    Deleted.

--- src/pages/api/getskinbyname.ts
+++
@@ -1,8 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {getSkinByName} from "../../../api/valorantSkins";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const skinName = JSON.parse(req.body).skinName;
-    const skin = await getSkinByName(skinName);
-    res.status(200).json({skin, loaded: true})
-}
